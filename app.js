a59732c5ff125160ca70de24827e36ea768a5e70
Marcel Teruo Nakamine <marcel.nakamine@outlook.com>
2018-08-29 15:17:25 -0300

    Adding Access Control Allow Origin

--- app.js
+++ app.js
@@ -34,6 +34,13 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Cross Origin middleware
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
+
 app.use('/', indexRouter);
 app.use('/api/v1/users', usersRouter);
 app.use('/api/v1/item', itemsRouter);
